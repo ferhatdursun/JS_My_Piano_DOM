1ae4e05e7c50b8f5bef710b03d14440ebdb8520b
Ferhat Dursun <ferhatdursnu@gmail.com>
2022-06-26 15:53:38 +0200

    JS

---
+++ app.js
@@ -0,0 +1,87 @@
+const q = document.querySelector("#Q");
+const a = document.querySelector("#A");
+const s = document.querySelector("#S");
+const d = document.querySelector("#D");
+const f = document.querySelector("#F");
+const g = document.querySelector("#G");
+const h = document.querySelector("#H");
+const j = document.querySelector("#J");
+const k = document.querySelector("#K");
+const l = document.querySelector("#L");
+const ö = document.querySelector("#Ö");
+const ä = document.querySelector("#Ä");
+const ü = document.querySelector("#Ü");
+
+
+
+const key_1 = document.querySelector(".key_1");
+const key_2 = document.querySelector(".key_2");
+const key_3 = document.querySelector(".key_3");
+const key_4 = document.querySelector(".key_4");
+const key_5 = document.querySelector(".key_5");
+const key_6 = document.querySelector(".key_6");
+const key_7 = document.querySelector(".key_7");
+const key_8 = document.querySelector(".key_8");
+const key_9 = document.querySelector(".key_9");
+const key_10 = document.querySelector(".key_10");
+const key_11 = document.querySelector(".key_11");
+const key_12 = document.querySelector(".key_12");
+const key_13 = document.querySelector(".key_13");
+
+
+
+
+q.onclick = () => {
+    key_1.play()
+}
+
+a.onclick = () => {
+    key_2.play()
+}
+s.onclick = () => {
+    key_3.play()
+}
+d.onclick = () => {
+    key_4.play()
+}
+f.onclick = () => {
+    key_5.play()
+}
+g.onclick = () => {
+    key_6.play()
+}
+h.onclick = () => {
+    key_7.play()
+}
+j.onclick = () => {
+    key_8.play()
+}
+k.onclick = () => {
+    key_9.play()
+}
+l.onclick = () => {
+    key_10.play()
+}
+ö.onclick = () => {
+    key_11.play()
+}
+ä.onclick = () => {
+    key_12.play()
+}
+
+ü.onclick = () => {
+    key_13.play()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
